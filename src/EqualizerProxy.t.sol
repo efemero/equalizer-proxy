4ef2e6c93ddfca7865d28465bf5e0cd6509664a6
François Bastien <fmrbastien@gmail.com>
2019-01-17 14:06:00 +0100

    initial commit

---
+++ src/EqualizerProxy.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24;
+
+import "ds-test/test.sol";
+
+import "./EqualizerProxy.sol";
+
+contract EqualizerProxyTest is DSTest {
+    EqualizerProxy proxy;
+
+    function setUp() public {
+        proxy = new EqualizerProxy();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
